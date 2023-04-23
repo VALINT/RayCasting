06945ee5746ce6e8f6fb35a154fbe90284c2835e
VALINT <33782876+VALINT@users.noreply.github.com>
2023-04-23 16:18:24 +0300

    Add project files.

---
+++ RayCasting/RayCasting.cpp
@@ -0,0 +1,12 @@
+﻿// RayCasting.cpp : Defines the entry point for the application.
+//
+
+#include "RayCasting.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
